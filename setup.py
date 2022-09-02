b9ac5e6fbb972cba542200efef000252c8f731d4
tek <tek@randhome.io>
2022-09-02 16:36:00 +0200

    Bumps version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='apkcli',
-    version='0.1.2',
+    version='0.1.3',
     description='Another APK info tool',
     long_description=long_description,
     long_description_content_type="text/markdown",
